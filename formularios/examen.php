bed254a40f01939460ccce770772e837daf90e72
ponchu <dancaballeroc@gmail.com>
2016-02-28 01:33:01 -0600

    Agregadas vista de examen y de resultado del mismo

---
+++ formularios/examen.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>TIES-OUT::Examen</title>
+		<link rel="shortcut icon" href="../img/logoTieOut.ico" type="image/x-icon" /> 	
+		<link rel="stylesheet" type="text/css" href="../css/repertorio.css">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<?php include_once("../imports.php"); ?>
+	</head>
+	<body>
+		<?php  include_once("../header.php"); ?>
+		<div class="content">
+			<section class="titulo" style="padding-top: 2em;">
+				<h2 class="text-center">Examen del curso "Árboles"</h2>
+			</section>
+			<section class="preguntas">
+				<div class="row" style="padding:0.5em 0em 0em 3em;">
+					<article class="pregunta">
+						<div class="col-md-12 text-center">
+							<h4>¿Qué es un árbol de búsqueda?</h4>
+								<label for="Opcion1">Opción 1 </label>
+							<input type="radio" name="respuesta1" id="Opcion1"></input><br>
+								<label for="Opcion2">Opción 2 </label>
+							<input type="radio" name="respuesta1" id="Opcion2"></input><br>
+								<label for="Opcion3">Opción 3 </label>
+							<input type="radio" name="respuesta1" id="Opcion3"></input><br>
+						</div>
+					</article>
+					<article class="pregunta">
+						<div class="col-md-12 text-center">
+							<h4>¿Qué es un árbol de búsqueda?</h4>
+								<label for="Opcion1">Opción 1 </label>
+							<input type="radio" name="respuesta2" id="Opcion1"></input><br>
+								<label for="Opcion2">Opción 2 </label>
+							<input type="radio" name="respuesta2" id="Opcion2"></input><br>
+								<label for="Opcion3">Opción 3 </label>
+							<input type="radio" name="respuesta2" id="Opcion3"></input><br>
+						</div>
+					</article>
+					<article class="pregunta">
+						<div class="col-md-12 text-center">
+							<h4>¿Qué es un árbol de búsqueda?</h4>
+								<label for="Opcion1">Opción 1 </label>
+							<input type="radio" name="respuesta3" id="Opcion1"></input><br>
+								<label for="Opcion2">Opción 2 </label>
+							<input type="radio" name="respuesta3" id="Opcion2"></input><br>
+								<label for="Opcion3">Opción 3 </label>
+							<input type="radio" name="respuesta3" id="Opcion3"></input><br>
+						</div>
+					</article>
+					<article class="pregunta">
+						<div class="col-md-12 text-center">
+							<h4>¿Qué es un árbol de búsqueda?</h4>
+								<label for="Opcion1">Opción 1 </label>
+							<input type="radio" name="respuesta4" id="Opcion1"></input><br>
+								<label for="Opcion2">Opción 2 </label>
+							<input type="radio" name="respuesta4" id="Opcion2"></input><br>
+								<label for="Opcion3">Opción 3 </label>
+							<input type="radio" name="respuesta4" id="Opcion3"></input><br>
+						</div>
+					</article>
+				</div>				
+			</section>
+			<section class="enviar" style="padding:3.5em 0em 0em 40%;">
+				<button class="btn btn-default" type="submit" >Enviar Respuestas</button>				
+			</section>
+
+
+		</div>
+	</body>
+</html>
