1060fc2544ecc4ba14aae4e1e9d463ff143714e2
Vetrom <noelvela94@gmail.com>
2016-02-29 08:12:18 -0600

    Agregado informacion.php

---
+++ informacion.php
@@ -0,0 +1,26 @@
+<html>
+	<head>
+		<title>TIES-OUT::Árboles</title>
+		<link rel="stylesheet" type="" href="css/curso1.css">
+		<link rel="shortcut icon" href="img/logoTieOut.ico" type="image/x-icon" /> 
+		<?php include_once("imports.php"); ?>
+	</head>
+	<body>
+		<?php include_once('header.php'); ?>
+		<div class="content">
+			<section>
+				<h2 id="titulo_contenido">QUIENES SOMOS</h2>
+			</section>
+
+			<section class="contenido">
+				<p align="justify">
+					TIES-OUT es el resultado del conjunto de varias ideas que surgieron con el proposito de ayudar y de orientar
+					a la gente que busca la guia y direccion para salir con las herramientas necesarias para conseguir salir de forma exitosa de su entrevista de trabajo.</br> </br>
+					Apoyandonos en la experiencia propia y en el conocimiento aplicado con exito en varios contextos, TIES-OUT ofrece los cursos necesarios para que nuestros usuarios se sientan preparados tanto en su forma profesional y el como manejan las distintas situaciones que se pueden presentar durante una entrvista de trabajo.  
+				</p>
+				
+			</section>
+
+		</div>
+	</body>
+</html>>
