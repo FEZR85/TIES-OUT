0a54ad9415ab756b363840a26c2d24cff3097b03
ponchu <dancaballeroc@gmail.com>
2016-04-17 22:49:16 -0500

    Implementada validacion de formulario en index de panel

--- panel/index.php
+++ panel/index.php
@@ -4,6 +4,7 @@
 		<link rel="shortcut icon" href="../img/logoTieOut.ico" type="image/x-icon" /> 
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link type="text/css" rel="stylesheet" href="css/index.css">
+		<link rel="stylesheet" type="text/css" href="css/sesion.css">
 		<?php include_once("imports.php"); ?>
 	</head>
 	<body>
@@ -12,14 +13,16 @@
 			<h2><img src="img/logoTieOut.png" alt="Logo" width="35em" >  TIES-OUT</h2>
 			<h3 class="encabezado text-center">Iniciar Sesión</h3><br>
 			<div class="row">
-				<form method="post" id="loginPanel" name="loginPanel" class="form-horizontal col-xs-offset-1 col-xs-10 col-sm-offset-2 col-sm-9 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6" role="form" novalidate >
+				<form method="post" id="formulario" name="loginPanel" class="form-horizontal col-xs-offset-1 col-xs-10 col-sm-offset-2 col-sm-9 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6" role="form" novalidate >
 					<div class="form-group">
 						<label for="correo">Correo </label>
 							<input id="correo" class="form-control" type="email" placeholder="alguien@ejemplo.com" />	
+							<span id="errorCorreo" class="clsError"></span>
 					</div>
 					<div class="form-group">							
-						<label for="password">Contraseña </label>
-							<input id="password" class="form-control" type="password" placeholder="contraseña" />		
+						<label for="contrasena">Contraseña </label>
+							<input id="contrasena" class="form-control" type="password" placeholder="contraseña" />		
+							<span id="errorContrasena" class="clsError"></span>	
 					</div>
 					
 					<div class="form-group text-center">
@@ -38,5 +41,6 @@
 		    	<p class="text-muted credit">TIES-OUT © 2016 | Contacto: contacto@ties-out.com</p>
 		  	</div>
 		</footer>
+		<script type="text/javascript" src="<?php echo ROOTPATH ?>/js/valida.js" ></script>
 	</body>
 </html>
