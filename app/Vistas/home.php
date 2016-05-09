176536df2b30579bdeb5b464a094b7cfdffa26e2
ponchu <dancaballeroc@gmail.com>
2016-05-08 22:57:04 -0500

    Corrección de redifinicion de constante roothpath

--- app/Vistas/home.php
+++ app/Vistas/home.php
@@ -1,9 +1,3 @@
-<?php 
-    $base_dir  = __DIR__; // Absolute path to your installation, ex: /var/www/mywebsite
-    $doc_root  = preg_replace("!${_SERVER['SCRIPT_NAME']}$!", '', $_SERVER['SCRIPT_FILENAME']);
-    $base_url  = preg_replace("!^${doc_root}!", '', $base_dir); # ex: '' or '/mywebsite'
-    define('ROOTPATH', $base_url); 
-?>
 
 <!DOCTYPE html>
 <html>
