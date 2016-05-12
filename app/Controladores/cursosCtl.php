a3526578d680d0e5d4d6d5c93b2ad1f9f262e9b2
ponchu <dancaballeroc@gmail.com>
2016-05-11 21:52:11 -0500

    Implementada accion de mostrar algunos cursos por medio del controlador

--- app/Controladores/cursosCtl.php
+++ app/Controladores/cursosCtl.php
@@ -11,31 +11,37 @@
 			echo "Curso.php";
 		}
 
-		public function ejecutar(){
-			//require_once("modelo/usuario.php");
-			//$this->modelo = new UsuarioMdl();
-			switch ($_GET['act']) {
-				case 'alta':
-						echo "altaAlumnos.php";
-					
+			public function ejecutar(){
+				//require_once("modelo/usuario.php");
+				//$this->modelo = new UsuarioMdl();
+				if(isset($_GET['act']) && isset($_GET['idcurso'])){
+					$idcurso = $_GET['idcurso'];
+					switch ($_GET['act']) {
+						case 'muestra':
+								$this->muestraCurso($idcurso);
+							break;
+						
+						default:
+							# code...
+							break;
+					}
+				}
+			}
+
+		private function muestraCurso($idcurso){
+
+			switch ($idcurso) {
+				case 1:
+					require('app/Vistas/curso1.php');
 					break;
-				case 'ver':
-						echo "perfilPublico.php";
+				case 2:
+					require('app/Vistas/curso2.php');
 					break;
-				case 'configurar':
-						echo "configurarPerfil.php";
-					break;
-				case 'recordar':
-						echo "recordar.php";
-					break;
-				case 'misCursos':
-						echo "misCursos.php";
-					break;
-				break;
 				default:
 					# code...
 					break;
 			}
+			
 		}
 	}
 ?>
