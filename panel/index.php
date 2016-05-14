37e99b4e24bb621ecae084d6bcbd11b74074ae34
FEZR85 <fzarater@gmail.com>
2016-05-14 15:58:10 -0500

    acomode el panel, aun no funciona

--- panel/index.php
+++ panel/index.php
@@ -2,31 +2,6 @@
 	$bandera = false;
 	if(isset($_GET['controlador'])){
 		switch ($_GET['controlador']) {
-			case 'administrador':
-					require('app/Controladores/administradorCtl.php');
-					$controlador = new Administrador();
-					$bandera = true;
-				break;
-			case 'busqueda':
-					require('app/Controladores/busquedaCtl.php');
-					$controlador = new Busqueda();
-					$bandera = true;
-				break;
-			case 'cursos':
-					require('app/Controladores/cursosCtl.php');
-					$controlador = new Curso();
-					$bandera = true;
-				break;
-			case 'general':
-					require('app/Controladores/generalCtl.php');
-					$controlador = new General();
-					$bandera = true;
-				break;
-			case 'usuarios':
-					require('app/Controladores/usuariosCtl.php');
-					$controlador = new Usuario();
-					$bandera = true;
-				break;
 			default:
 				# code...
 					require_once('Vistas/home.html');
