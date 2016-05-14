08f6c20e630f12b0b9185694a790ae0bfeddc96d
FEZR85 <fzarater@gmail.com>
2016-05-14 15:26:21 -0500

    agregue index para panel

---
+++ panel/index.php
@@ -0,0 +1,42 @@
+<?php
+	$bandera = false;
+	if(isset($_GET['controlador'])){
+		switch ($_GET['controlador']) {
+			case 'administrador':
+					require('app/Controladores/administradorCtl.php');
+					$controlador = new Administrador();
+					$bandera = true;
+				break;
+			case 'busqueda':
+					require('app/Controladores/busquedaCtl.php');
+					$controlador = new Busqueda();
+					$bandera = true;
+				break;
+			case 'cursos':
+					require('app/Controladores/cursosCtl.php');
+					$controlador = new Curso();
+					$bandera = true;
+				break;
+			case 'general':
+					require('app/Controladores/generalCtl.php');
+					$controlador = new General();
+					$bandera = true;
+				break;
+			case 'usuarios':
+					require('app/Controladores/usuariosCtl.php');
+					$controlador = new Usuario();
+					$bandera = true;
+				break;
+			default:
+				# code...
+					require_once('Vistas/home.html');
+				break;
+		}
+
+		if($bandera){
+			$controlador->ejecutar();
+		}
+	}else{
+		require_once('Vistas/home.html');
+	}
+?>
