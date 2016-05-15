32428597f398d602855c97c6b6fe41bf09e5d6d8
ponchu <dancaballeroc@gmail.com>
2016-05-14 20:15:58 -0500

    Implementada vista de misCursos a controlador cursos

--- app/Controladores/cursosCtl.php
+++ app/Controladores/cursosCtl.php
@@ -6,42 +6,59 @@
 	{
 
 		private $modelo;
+		private $header;
+		private $footer;
 
-		function muestra(){
-			echo "Curso.php";
+		function __construct(){
+			$this->header = file_get_contents("app/Vistas/header.html");
+			$this->footer = file_get_contents("app/Vistas/footer.html");			
 		}
 
-			public function ejecutar(){
+		public function ejecutar(){
 				//require_once("modelo/usuario.php");
 				//$this->modelo = new UsuarioMdl();
-				if(isset($_GET['act']) && isset($_GET['idcurso'])){
-					$idcurso = $_GET['idcurso'];
+				if(isset($_GET['act'])){
+					if(isset($_GET['idcurso'])){
+						$idcurso = $_GET['idcurso'];
+					}else{
+						$idcurso = -1;
+					}
+
+					
 					switch ($_GET['act']) {
-						case 'muestra':
+						case 'mostrar':
 								$this->muestraCurso($idcurso);
 							break;
-						
+						case 'miscursos':
+								$this->misCursos();
+							break;
 						default:
 							# code...
 							break;
 					}
 				}
-			}
+		}
 
 		private function muestraCurso($idcurso){
+			if($idcurso>=0){
+				switch ($idcurso) {
+					case 1:
+						require('app/Vistas/curso1.php');
+						break;
+					case 2:
+						require('app/Vistas/curso2.php');
+						break;
+					default:
+						# code...
+						break;
+				}
+			}			
+		}
 
-			switch ($idcurso) {
-				case 1:
-					require('app/Vistas/curso1.php');
-					break;
-				case 2:
-					require('app/Vistas/curso2.php');
-					break;
-				default:
-					# code...
-					break;
-			}
-			
+		private function misCursos(){
+			$vista = file_get_contents('app/Vistas/misCursos.html');
+
+			echo $this->header . $vista . $this->footer;
 		}
 	}
 ?>
