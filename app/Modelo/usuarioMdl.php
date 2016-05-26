51c59ac80e8be7305e87acfedff702415e075b68
ponchu <dancaballeroc@gmail.com>
2016-05-26 11:25:06 -0500

    Ajuste a inserción en la base nueva

--- app/Modelo/usuarioMdl.php
+++ app/Modelo/usuarioMdl.php
@@ -11,8 +11,8 @@ class UsuarioMdl{
 		/*$query = "INSERT INTO usuario(vchNombre, vchPaterno, vchMaterno, vchCorreo, vchContraseña,tiEliminado, tiActivo, iidTipo)
 				VALUES('$nombre','uno','dos','$correo','$contrasena',0,1,1)";*/
 		//La siguiente query es segun la otra base de datos
-		$query = "INSERT INTO usuario(vchnombre, vchpaterno, vchmaterno, vchcorreo, vchpassword, iTipo)
-				VALUES('$nombre','uno','dos','$correo','$contrasena',1)";
+		$query = "INSERT INTO usuario (vchNombre,vchPaterno, vchMaterno,vchCorreo, vchContrasena, tiEliminado, tiActivo, iidTipo) VALUES ('$nombre','uno','dos','$correo','$contrasena',0,1,1)";
+				echo $query;
 		$result = $this->mysql->query($query);
 		if($this -> mysql-> insert_id){
 			return $this -> mysql -> insert_id;
