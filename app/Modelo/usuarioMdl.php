63e3a92fc8c6aa67ca28eb134e81da8f1ebf4865
FEZR85 <fzarater@gmail.com>
2016-05-28 00:57:11 -0500

    Modifique y agregue consultas

--- app/Modelo/usuarioMdl.php
+++ app/Modelo/usuarioMdl.php
@@ -11,8 +11,7 @@ class UsuarioMdl{
 		/*$query = "INSERT INTO usuario(vchNombre, vchPaterno, vchMaterno, vchCorreo, vchContraseña,tiEliminado, tiActivo, iidTipo)
 				VALUES('$nombre','uno','dos','$correo','$contrasena',0,1,1)";*/
 		//La siguiente query es segun la otra base de datos
-		$query = "INSERT INTO usuario(vchnombre, vchpaterno, vchmaterno, vchcorreo, vchpassword, iTipo)
-				VALUES('$nombre','uno','dos','$correo','$contrasena',1)";
+		$query = "INSERT INTO usuario (vchNombre,vchPaterno, vchMaterno,vchCorreo, vchContrasena, tiEliminado, tiActivo, iidTipo) VALUES ('$nombre','uno','dos','$correo','$contrasena',0,1,1)";
 		$result = $this->mysql->query($query);
 		if($this -> mysql-> insert_id){
 			return $this -> mysql -> insert_id;
@@ -22,14 +21,21 @@ class UsuarioMdl{
 	}
 
 	function consultaUsuario($correo, $contrasena){
-		$query = $this->mysql->prepare('SELECT vchnombre FROM usuario WHERE vchcorreo=$correo AND vchpassword=$contrasena');
+		$query = "SELECT vchnombre FROM usuario WHERE vchcorreo='$correo' AND vchcontrasena='$contrasena' AND tiActivo = 1";
+		$resultado = $this->mysql->query($query);
+		while($fila = $resultado->fetch_assoc()){
+			$info[] = $fila;
+		}
+		return $info;
+	}
 
-			$query->execute();
-			$query->store_result();
-			$query->fetch();
-
-			echo $query;
-
-		//$result = $this->mysql-
+	function existecorreo($correo){
+		$query = "SELECT vchCorreo FROM usuario WHERE vchCorreo = '$correo' AND tiActivo = 1 AND tiEliminado = 0";
+		$resultado = $this->mysql->query($query);
+		while($fila = $resultado->num_rows){
+			if($fila > 0)
+				return true;
+		}
+		return false;
 	}
 }
