f67b0f8d95ea353c371f78f9e0d51cf88c9fd0a0
FEZR85 <fzarater@gmail.com>
2016-05-29 13:29:59 -0500

    agregue validacion de sesion en contructor

--- app/Controladores/busquedaCtl.php
+++ app/Controladores/busquedaCtl.php
@@ -1,6 +1,6 @@
-<?php  
+<?php
 	/**
-	* 
+	*
 	*/
 	class Busqueda
 	{
@@ -9,29 +9,21 @@
 
 		function __construct(){
 			session_start();
+			if($_SESSION["iidUsuario"] == "" || isset($_SESSION["iidUsuario"]))
+				$this->header = file_get_contents("app/Vistas/header.html");
+			else
+				$this->header = file_get_contents("app/Vistas/header2.html");
+			$this->footer = file_get_contents("app/Vistas/footer.html");
+			$this->head = file_get_contents("app/Vistas/head.html");
 		}
 
 		public function ejecutar(){
 			//require_once("modelo/usuario.php");
 			//$this->modelo = new UsuarioMdl();
 			switch ($_GET['act']) {
-				case 'alta':
-						echo "altaAlumnos.php";
-					
+				case 'buscar':
+						//$this->mostrarPerfil($idUsuario);
 					break;
-				case 'ver':
-						echo "perfilPublico.php";
-					break;
-				case 'configurar':
-						echo "configurarPerfil.php";
-					break;
-				case 'recordar':
-						echo "recordar.php";
-					break;
-				case 'misCursos':
-						echo "misCursos.php";
-					break;
-				break;
 				default:
 					# code...
 					break;
