feef7240fff993ef2d087be4adef5fa21a366507
FEZR85 <fzarater@gmail.com>
2016-05-29 13:27:29 -0500

    agregue validacion en el constructor

--- app/Controladores/administradorCtl.php
+++ app/Controladores/administradorCtl.php
@@ -9,6 +9,12 @@
 
 		function __construct(){
 			session_start();
+			if($_SESSION["iidUsuario"] == "" || isset($_SESSION["iidUsuario"]))
+				$this->header = file_get_contents("app/Vistas/header.html");
+			else
+				$this->header = file_get_contents("app/Vistas/header2.html");
+			$this->footer = file_get_contents("app/Vistas/footer.html");
+			$this->head = file_get_contents("app/Vistas/head.html");
 		}
 
 		public function ejecutar(){
