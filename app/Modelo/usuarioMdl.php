dadfda9ecce6b0095a8b4c0311f0463347ed9768
ponchu <dancaballeroc@gmail.com>
2016-05-29 01:13:41 -0500

    Corregida inserción que no insertaba correctamente a la base

--- app/Modelo/usuarioMdl.php
+++ app/Modelo/usuarioMdl.php
@@ -11,7 +11,7 @@ class UsuarioMdl{
 		/*$query = "INSERT INTO usuario(vchNombre, vchPaterno, vchMaterno, vchCorreo, vchContraseña,tiEliminado, tiActivo, iidTipo)
 				VALUES('$nombre','uno','dos','$correo','$contrasena',0,1,1)";*/
 		//La siguiente query es segun la otra base de datos
-		$query = "INSERT INTO usuario (vchNombre,vchCorreo, vchContrasena) VALUES ('$nombre','$contrasena')";
+		$query = "INSERT INTO usuario (vchNombre,vchPaterno, vchMaterno,vchCorreo, vchContrasena, tiEliminado, tiActivo, iidTipo) VALUES ('$nombre','uno','dos','$correo','$contrasena',0,1,1)";
 		$result = $this->mysql->query($query);
 		if($this -> mysql-> insert_id){
 			return $this -> mysql -> insert_id;
