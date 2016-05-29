2aab6d38364f6861619c80f517e9154ee4797c06
FEZR85 <fzarater@gmail.com>
2016-05-29 13:33:38 -0500

    quite el ajax

--- recursos/js/valida.js
+++ recursos/js/valida.js
@@ -1,5 +1,4 @@
 /**************************************Inicio de sesion*****************************************/
-    var banderaRegistro = 0;//variable para usar ajax
     var formulario = document.querySelector("#formulario"), //Tomo al formulario
     	mensajesError = jQuery(".clsError");
    		patronCorreo = /^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/i, //Expresión regular de validación del campo Correo
@@ -26,7 +25,6 @@
 	     				jQuery(mensajesError[i]).text("El correo es incorrecto");
      				}else{
      					jQuery(mensajesError[i]).css('display','none');
-                        banderaRegistro = 1;//variable para usar ajax
      				}
      			}else if(formulario[i].id == "contrasena" || formulario[i].id == "rptContrasena"){//Caso especial para campo contaseña
      				if(!patronContrasena.test(formulario[i].value)){
@@ -38,7 +36,6 @@
      						if(contrasena == formulario[i].value){
      							campoCorrecto = true;
      							jQuery(mensajesError[i]).css('display','none');
-                                banderaRegistro = 3;//variable para usar ajax
      						}else{
      							campoCorrecto = false;
      							jQuery(mensajesError[i]).css('display','inline');
@@ -47,7 +44,6 @@
      					}else{
      						contrasena = formulario[i].value;
      						jQuery(mensajesError[i]).css('display','none');
-                            banderaRegistro = 2;//variable para usar ajax
      					}
      				}
      			}else if(formulario[i].id == "nombre" || formulario[i].id == "ocupacion"){//Caso para campos de solo letras
@@ -62,11 +58,6 @@
 
      			}
      		}
-
-            if(banderaRegistro == 3){//funcion nueva para usar el ajax
-                enviar = false;
-                registro();
-            }
 
      		if(!campoCorrecto){
      			jQuery(formulario[i]).addClass("campoError");
@@ -84,21 +75,3 @@
      	}
 
     }, false);
-
-function registro(){//funcion para usar ajax que no funciona
-    var dataString = $("#formulario").serialize();
-    $.ajax({
-        type: "POST",
-        url: "index.php?controlador=usuarios&act=registrar",
-        data: dataString,
-        success: function(data){
-                $("#miscursos").css('display','inline-block');
-                $("#registro").css('display','none');
-                $("#entrar").css('display','none');
-                $("#infoUser").css('display','inline-block');
-                $("#confPerfil").css('display','inline-block');
-                $("#salir").css('display','inline-block');
-
-        }
-    });
-}
