9499118d9dea7fbb2620e7d7ccae7623a02a80d6
FEZR85 <fzarater@gmail.com>
2016-05-30 13:12:52 -0500

    ya muestras los cursos existentes en la BD

--- panel/Controladores/cursosCtl.php
+++ panel/Controladores/cursosCtl.php
@@ -9,10 +9,16 @@
 		private $header;
 		private $footer;
 		private $head;
+		private $instancia;
+		private $mysql;
+		private $generalctl;
 
 		function __construct(){
 			require('Controladores/generalCtl.php');
+			$this->instancia = Conexion::getInstance();
+			$this->instancia->__construct();
 
+			$this->mysql = $this->instancia->getConnection();
 // 			session_start();
 			$this->header = file_get_contents("Vistas/header.html");
 			$this->footer = file_get_contents("Vistas/footer.html");
@@ -33,6 +39,9 @@
 						case 'agregar':
 								$this->agregarCursos();
 							break;
+						case 'guardar':
+								$this->guardarCursos();
+							break;
 						default:
 							# code...
 							break;
@@ -42,17 +51,81 @@
 
 		private function muestraCursos(){
 
-			$vista = file_get_contents("Vistas/listas/cursos.html");
+			require('Modelo/cursosMdl.php');
+			$this->modelo = new CursosMdl($this->mysql);
+
+			$diccionario = "";
+			$diccionarioCuros = "";
+			$filas = "";
+			$vista = file_get_contents('Vistas/listas/cursos.html');
+			$inicio_fila = strrpos($vista,'<!--{cursos}-->');
+			$final_fila = strrpos($vista,'<!--{cursosT}-->') + 16;
+			$fila = substr($vista,$inicio_fila,$final_fila-$inicio_fila);
+			//Genero las filas
+			$alumnos = $this->modelo->traerCursos();
+			foreach ($alumnos as $row) {
+				$new_fila = $fila;
+				$diccionarioCursos = array(
+					'<!--{nombre}-->' => $row['nombre'],
+					'<!--{descripcion}-->' => $row['contenido']);
+					$new_fila = strtr($new_fila,$diccionarioCursos);
+					$filas .= $new_fila;
+				}
+
+			//Reemplazo en mi vista una fila por todas las filas
+			$vista = str_replace($fila, $filas, $vista);
 			$diccionario = array(
-			'{tituloPagina}'=>"Cursos",
-			'<!--{otros}-->' => '<link rel="stylesheet" type="text/css" href="../recursos/css/panel/simple-sidebar.css">');
+				'{tituloPagina}'=> "Cursos",
+				'<!--{masLinks}-->'=> '<link rel="stylesheet" type="text/css" href="../recursos/css/panel/simple-sidebar.css"/>');
+
 			$this->head = strtr($this->head,$diccionario);
-			echo $this->head . $this->header . $vista . $this->footer;
+			$vista = $this->head . $this->header . $vista . $this->footer;
+
+			echo $vista;
 		}
 
 		private function agregarCursos(){
 
 			$vista = file_get_contents("Vistas/agregarcurso.html");
+			$diccionario = array(
+			'{tituloPagina}'=>"Agregar cursos",
+			'<!--{otros}-->' => '<link rel="stylesheet" type="text/css" href="../recursos/css/panel/simple-sidebar.css">');
+			$this->head = strtr($this->head,$diccionario);
+			echo $this->head . $this->header . $vista . $this->footer;
+		}
+
+		private function guardarCursos(){
+			$titulo = "";
+			$contenido = "";
+			require('Modelo/cursosMdl.php');
+			$this->modelo = new CursosMdl($this->mysql);
+
+			if(empty($_POST)){
+				$this->mostrarProblema("Llene los campos para guardar el curso.");
+			}else{
+				$titulo = $_POST['titulo'];
+				$contenido = $_POST['contenido'];
+
+				$resultado = $this->modelo->guardarCurso($titulo, $contenido);
+				if($resultado!==FALSE){
+					$vista = file_get_contents("Vistas/listas/cursos.html");
+					$diccionario = array(
+					'{tituloPagina}'=>"Cursos",
+					'<!--{otros}-->' => '<link rel="stylesheet" type="text/css" href="../recursos/css/panel/simple-sidebar.css">');
+					$this->head = strtr($this->head,$diccionario);
+					echo $this->head . $this->header . $vista . $this->footer;
+				}
+				else {
+					$this->mostrarProblema("No se pudo guardar el curso. Intente más tarde.");
+				}
+			}
+		}
+
+		private function mostrarProblema($string){
+			$vista = file_get_contents("Vistas/agregarcurso.html");
+			$diccionarioP = array(
+			'<!--{problema}-->' => '<h4 class="text-danger">'.$string.'</h4>');
+			$vista = strtr($vista,$diccionarioP);
 			$diccionario = array(
 			'{tituloPagina}'=>"Agregar cursos",
 			'<!--{otros}-->' => '<link rel="stylesheet" type="text/css" href="../recursos/css/panel/simple-sidebar.css">');
